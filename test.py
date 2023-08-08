b60b8bb9029f76a7f60f4093bdf5f0a98c961699
chandhrunewron <141120584+chandhrunewron@users.noreply.github.com>
2023-08-08 11:06:18 +0530

    Update test.py

--- test.py
+++ test.py
@@ -8,3 +8,30 @@
 # res = requests.post('http://localhost:8000/', json = model_inputs)
 
 # print(res.json())
+import banana_dev as client
+from io import BytesIO
+from PIL import Image
+import time
+
+# Localhost test
+my_model = client.Client(
+    api_key="",
+    model_key="",
+    url="http://localhost:8000",
+)
+
+inputs = {
+    "prompt": "Tell me about AI.",
+    "max_new_tokens": 128
+}
+
+# Call your model's inference endpoint on Banana.
+# If you have set up your Potassium app with a
+# non-default endpoint, change the first
+# method argument ("/")to specify a
+# different route.
+t1 = time.time()
+result, meta = my_model.call("/", inputs)
+t2 = time.time()
+print("Time to run: ", t2 - t1)
+print(result)
