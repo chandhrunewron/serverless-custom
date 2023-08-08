3e216f6c3d7eec15b2d77b83d70639abf9cf3c81
chandhrunewron <141120584+chandhrunewron@users.noreply.github.com>
2023-08-08 10:03:56 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,11 +1,11 @@
-import banana_dev as banana
-import os
+# import banana_dev as banana
+# import os
 
-api_key = os.environ.get("API_KEY")
-model_key = os.environ.get("MODEL_KEY")
+# api_key = os.environ.get("API_KEY")
+# model_key = os.environ.get("MODEL_KEY")
 
-model_inputs = {'prompt': 'My favorite part about working with AI is'}
+# model_inputs = {'prompt': 'My favorite part about working with AI is'}
 
-model_outputs = banana.run(api_key, model_key, model_inputs)
+# model_outputs = banana.run(api_key, model_key, model_inputs)
 
-print(model_outputs)
+# print(model_outputs)
