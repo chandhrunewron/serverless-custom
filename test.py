fb8879b7acd399432882efd6f0ec6b17eaab3113
chandhrunewron <141120584+chandhrunewron@users.noreply.github.com>
2023-08-08 10:04:34 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
-# This file is used to verify your http server acts as expected
-# Run it with `python3 test.py``
+# # This file is used to verify your http server acts as expected
+# # Run it with `python3 test.py``
 
-import requests
+# import requests
 
-model_inputs = {'prompt': 'My favorite part about working with AI is'}
+# model_inputs = {'prompt': 'My favorite part about working with AI is'}
 
-res = requests.post('http://localhost:8000/', json = model_inputs)
+# res = requests.post('http://localhost:8000/', json = model_inputs)
 
-print(res.json())
+# print(res.json())
